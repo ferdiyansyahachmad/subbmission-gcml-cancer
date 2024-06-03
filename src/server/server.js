0e4127bb50d85c728655cfb81008afe8cec95392
Ferdy <ferdiyansyah.achmad@mhs.politala.ac.id>
2024-06-03 11:44:36 +0800

    update server port and host

--- src/server/server.js
+++ src/server/server.js
@@ -7,8 +7,8 @@ const InputError = require("../exceptions/InputError");
 
 (async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
+    port: process.env.PORT || 3000,
+    host: process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost",
     routes: {
       cors: {
         origin: ["*"],
